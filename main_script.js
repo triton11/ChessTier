70ee501053c6a5d7f23ac2952c4cc5bf9aacd020
triton11 <tristrumtuttle@gmail.com>
2021-10-07 02:10:24 -0400

    clean up

--- main_script.js
+++ main_script.js
@@ -1,68 +1,103 @@
 // This is needed to prevent infinite looping when the mutation observer is
 // triggered.
-let global_elos = new Map();
+let globalRatings = new Map();
 
 function run() {
-  const elo = document.querySelectorAll('div.user-tagline-component')
-  for (let i = 0; i < elo.length; i++) {
-    const orig = elo[i].querySelector('span.user-tagline-rating')
-    if (orig !== null) {
-      let current_elo = orig.innerHTML.replace(/\D/g,'')
-      let num_elo = parseInt(current_elo, 10);
-      const name = elo[i].querySelector('a.user-username-component').innerHTML   
-      if (!isNaN(num_elo) && global_elos.get(name) !== num_elo) {
-        // orig.innerHTML = current_elo + "<span style='display:'> (" + calculateBlitzTier(num_elo) + ") </span>"
-        const childNodeText = document.createTextNode("(" + calculateBlitzTier(num_elo) + ")");
-        const childNode = document.createElement("span");
-        childNode.setAttribute("id", "previous-elo")
-        childNode.style.color = 'hsla(0,0%,100%,.65)'
-        childNode.appendChild(childNodeText)
-        const previousNode = elo[i].querySelector("#previous-elo")
-        if (previousNode !== null) {
-          elo[i].removeChild(previousNode);
+  const tagline = document.querySelectorAll('div.user-tagline-component');
+  for (let i = 0; i < tagline.length; i++) {
+    const rating = tagline[i].querySelector('span.user-tagline-rating');
+    if (rating !== null) {
+      const ratingNum = parseInt(rating.innerHTML.replace(/\D/g,''), 10);
+      const username = tagline[i].querySelector('a.user-username-component').innerHTML; 
+      if (!isNaN(ratingNum) && globalRatings.get(username) !== ratingNum) {
+        swapNewRating(tagline[i], rating, ratingNum, username)
+      }
+    }
+  }
+  const endRating = document.querySelector('div.rating-value-value');
+  if (endRating !== null) {
+    const rating = endRating.querySelector('span.new-rating-component')
+    const ratingNum = parseInt(rating.innerHTML.replace(/\D/g,''), 10);
+    const mapKey = 'endRating'
+    const oldRatingNum = globalRatings.get(mapKey)
+    if (!isNaN(ratingNum) && oldRatingNum !== ratingNum) {
+      swapNewRating(endRating, rating, ratingNum, mapKey)
+      const ratingChange = endRating.querySelector('div.rating-change-right')
+      if (!isNaN(oldRatingNum) && calculateBlitzTier(ratingNum) !== calculateBlitzTier(oldRatingNum)) {
+        if (oldRatingNum < ratingNum) {
+          ratingChange.style.display = "block";
+          ratingChange.innerHTML = 'Promoted!'
+        } else {
+          ratingChange.style.display = "block";
+          ratingChange.innerHTML = 'Demoted.'
         }
-        elo[i].insertBefore(childNode, orig)
-        orig.style.display = "none";
-        global_elos.set(name, num_elo)
+      } else {
+        ratingChange.style.display = "none";
       }
     }
   }
 }
 
-function calculateBlitzTier(num_elo) {
-  if (num_elo < 500) {
+function swapNewRating(parentNode, ratingNode, ratingNum, globalKey) {
+  const newTierNode = makeRatingNode(ratingNum, globalKey);
+  const previousTierNode = parentNode.querySelector('#previous-rating');
+  if (previousTierNode !== null) {
+    parentNode.removeChild(previousTierNode);
+  }
+  parentNode.insertBefore(newTierNode, ratingNode);
+  ratingNode.style.display = "none";
+  globalRatings.set(globalKey, ratingNum);
+}
+
+function makeRatingNode(ratingNum, globalKey) {
+  let childNodeText = document.createTextNode(`(${calculateBlitzTier(ratingNum)})`);
+  if (globalKey === 'endRating') {
+    childNodeText = document.createTextNode(calculateBlitzTier(ratingNum));
+  }
+  const childNode = document.createElement('span');
+  childNode.setAttribute('id', 'previous-rating')
+  childNode.style.color = 'hsla(0,0%,100%,.65)'
+  if (globalKey === 'endRating') {
+    childNode.style.color = '#000'
+  }
+  childNode.appendChild(childNodeText)
+  return childNode
+}
+
+function calculateBlitzTier(ratingNum) {
+  if (ratingNum < 500) {
     return "Bronze IV"
-  } else if (num_elo < 550) {
+  } else if (ratingNum < 550) {
     return "Silver I"
-  } else if (num_elo < 600) {
+  } else if (ratingNum < 600) {
     return "Silver II"
-  } else if (num_elo < 650) {
+  } else if (ratingNum < 650) {
     return "Silver III"
-  } else if (num_elo < 700) {
+  } else if (ratingNum < 700) {
     return "Silver IV"
-  } else if (num_elo < 775) {
+  } else if (ratingNum < 775) {
     return "Gold I"
-  } else if (num_elo < 850) {
+  } else if (ratingNum < 850) {
+    return "Gold II"
+  } else if (ratingNum < 925) {
     return "Gold III"
-  } else if (num_elo < 925) {
-    return "Gold III"
-  } else if (num_elo < 1000) {
+  } else if (ratingNum < 1000) {
     return "Gold IV"
-  } else if (num_elo < 1075) {
+  } else if (ratingNum < 1075) {
     return "Platinum I"
-  } else if (num_elo < 1150) {
+  } else if (ratingNum < 1150) {
     return "Platinum II"
-  } else if (num_elo < 1220) {
+  } else if (ratingNum < 1220) {
     return "Platinum III"
-  } else if (num_elo < 1300) {
+  } else if (ratingNum < 1300) {
     return "Platinum IV"
-  } else if (num_elo < 1400) {
+  } else if (ratingNum < 1400) {
     return "Diamond I"
-  } else if (num_elo < 1500) {
+  } else if (ratingNum < 1500) {
     return "Diamond II"
-  } else if (num_elo < 1600) {
+  } else if (ratingNum < 1600) {
     return "Diamond III"
-  } else if (num_elo < 1700) {
+  } else if (ratingNum < 1700) {
     return "Diamond IV"
   } else {
     return "Champion"
